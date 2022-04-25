d7f0b963de7699185ff6f6645b7dbb4b60f258b0
IsaakDl <Daniel.Isaak@pm.me>
2022-04-25 23:58:31 +0200

    Create ThumbParamsOSC.py

---
+++ src/ThumbParamsOSC.py
@@ -0,0 +1,77 @@
+import json
+import openvr
+import sys
+import os
+import time
+from pythonosc import udp_client
+
+debugenabled = False
+try:
+    debugenabled = True if sys.argv[1] == "--debug" else False
+except IndexError:
+    pass
+
+oscClient = udp_client.SimpleUDPClient("127.0.0.1", 9000)
+
+def resource_path(relative_path):
+    base_path = getattr(sys, '_MEIPASS', os.path.dirname(os.path.abspath(__file__)))
+    return os.path.join(base_path, relative_path)
+
+application = openvr.init(openvr.VRApplication_Utility)
+action_path = os.path.join(resource_path('bindings'), 'thumbparams_actions.json')
+openvr.VRInput().setActionManifestPath(action_path)
+actionSet_thumbparams = openvr.VRInput().getActionSetHandle('/actions/thumbparams')
+
+buttonActionHandles = []
+config = json.load(open(os.path.join(os.path.join(resource_path('config.json')))))
+for k in config["Buttons"]:
+    buttonActionHandles.append(openvr.VRInput().getActionHandle(config["Buttons"][k]))
+
+leftTrigger = openvr.VRInput().getActionHandle(config["Trigger"]["lefttrigger"])
+rightTrigger = openvr.VRInput().getActionHandle(config["Trigger"]["righttrigger"])
+
+def handle_input():
+    event = openvr.VREvent_t()
+    has_events = True
+    while has_events:
+        has_events = application.pollNextEvent(event)
+    actionSets = (openvr.VRActiveActionSet_t * 1)()
+    actionSet = actionSets[0]
+    actionSet.ulActionSet = actionSet_thumbparams
+    openvr.VRInput().updateActionState(actionSets)
+
+    lrInputs = ""
+    for i in buttonActionHandles:
+        lrInputs += str(openvr.VRInput().getDigitalActionData(i, openvr.k_ulInvalidInputValueHandle).bState)
+
+    leftThumb = lrInputs[:4].rfind("1") + 1
+    rightThumb = lrInputs[4:].rfind("1") + 1
+    
+    leftTriggerValue =  openvr.VRInput().getAnalogActionData(leftTrigger, openvr.k_ulInvalidInputValueHandle).x
+    rightTriggerValue =  openvr.VRInput().getAnalogActionData(rightTrigger, openvr.k_ulInvalidInputValueHandle).x
+                                             
+    oscClient.send_message("/avatar/parameters/LeftTrigger", float(leftTriggerValue))
+    oscClient.send_message("/avatar/parameters/RightTrigger", float(rightTriggerValue))
+    oscClient.send_message("/avatar/parameters/LeftThumb", int(leftThumb))
+    oscClient.send_message("/avatar/parameters/RightThumb", int(rightThumb))
+
+    if debugenabled:
+        print("=================")
+        print("Inputs:\t", lrInputs)
+        print("left:\t", leftThumb)
+        print("right:\t", rightThumb)
+        print("Tright:\t", rightTriggerValue)
+        print("Tleft:\t", leftTriggerValue)
+
+print("============================")
+print("VRCThumbParamsOSC running...")
+print("============================")
+print("You can minimize this window...")
+print("Press CTRL+C to exit or just close the window")
+
+while True:
+    try:
+        handle_input()
+        time.sleep(0.005)
+    except KeyboardInterrupt:
+        exit()

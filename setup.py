ffcfed1062d04db84b1ae50271f13651aaea924d
IsaakDl <Daniel.Isaak@pm.me>
2022-08-08 18:34:46 +0200

    Pyinstaller to CX_Freeze

---
+++ setup.py
@@ -0,0 +1,19 @@
+import sys
+from cx_Freeze import setup, Executable
+
+# Dependencies are automatically detected, but it might need fine tuning.
+# "packages": ["os"] is used as example only
+packages = ["os", "json", "traceback", "openvr", "sys", "time", "ctypes", "argparse", "pythonosc"]
+exclude = ["tkinter", "asyncio", "concurrent", "html", "http", "lib2to3", "multiprocessing", "test", "unittest", "xmlrpc"]
+file_include = ["config.json", "Run Debug Mode.bat", "openvr/", "bindings/"]
+bin_excludes = ["_bz2.pyd", "_decimal.pyd", "_hashlib.pyd", "_lzma.pyd", "_queue.pyd", "_ssl.pyd", "libcrypto-1_1.dll", "libssl-1_1.dll", "ucrtbase.dll", "VCRUNTIME140.dll"]
+
+build_exe_options = {"packages": packages, "excludes": exclude, "include_files": file_include, "bin_excludes": bin_excludes}
+
+setup(
+    name="ThumbParamsOSC",
+    version="0.3.1",
+    description="ThumbParamsOSC",
+    options={"build_exe": build_exe_options},
+    executables=[Executable("ThumbParamsOSC.py", base=False, icon="icon.ico")],
+)

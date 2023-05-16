d4918afc44ae4e3b68e577cdabf118596940171f
Isaak <43730681+I5UCC@users.noreply.github.com>
2023-05-17 00:30:42 +0200

    fixed building
    for some reason the program doesn't launch after building anymore without the "html" package, so i removed it from the excludes

--- src/setup.py
+++ src/setup.py
@@ -4,7 +4,7 @@ from cx_Freeze import setup, Executable
 # Dependencies are automatically detected, but it might need fine tuning.
 # "packages": ["os"] is used as example only
 packages = ["os", "json", "traceback", "openvr", "sys", "time", "ctypes", "argparse", "pythonosc"]
-exclude = ["tkinter", "asyncio", "concurrent", "html", "http", "lib2to3", "multiprocessing", "test", "unittest", "xmlrpc"]
+exclude = ["tkinter", "asyncio", "concurrent", "http", "lib2to3", "multiprocessing", "test", "unittest", "xmlrpc"]
 file_include = ["config.json", "ovrConfig.json", "Run Debug Mode.bat", "bindings/", "app.vrmanifest"]
 bin_excludes = ["_bz2.pyd", "_decimal.pyd", "_hashlib.pyd", "_lzma.pyd", "_queue.pyd", "_ssl.pyd", "libcrypto-1_1.dll", "libssl-1_1.dll", "ucrtbase.dll", "VCRUNTIME140.dll"]
 

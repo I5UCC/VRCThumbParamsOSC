1816a2074ea4f7076750118424979b4a97fda15f
Isaak <43730681+I5UCC@users.noreply.github.com>
2023-05-25 02:20:16 +0200

    rename Configurator
    it thinks it is a package some other projects use, so i decided to rename it to something else

--- build.bat
+++ build.bat
@@ -3,17 +3,17 @@
 cd %~dp0
 rmdir /S /Q src/build
 rmdir /S /Q build
-rmdir /S /Q src\Configurator\Configurator\bin
+rmdir /S /Q src\Thumbparams_Configurator\Thumbparams_Configurator\bin
 
-cd src/Configurator
+cd src/Thumbparams_Configurator
 dotnet publish -r win-x64
 cd ..
 "../venv/Scripts/python.exe" setup.py build
 cd build
 robocopy exe.win-amd64-3.10 ThumbparamsOSC /MOVE /E /NFL /NDL /NJH /NJS /nc /ns /np
 cd ..
-powershell -c "copy Configurator\Configurator\bin\Debug\net4.8.1-windows\win-x64\publish\Configurator.exe build/ThumbparamsOSC/Configurator.exe"
-powershell -c "copy Configurator\Configurator\bin\Debug\net4.8.1-windows\win-x64\publish\Newtonsoft.Json.dll build/ThumbparamsOSC/Newtonsoft.Json.dll"
+powershell -c "copy Thumbparams_Configurator\Thumbparams_Configurator\bin\Debug\net4.8.1-windows\win-x64\publish\Thumbparams_Configurator.exe build/ThumbparamsOSC/Configurator.exe"
+powershell -c "copy Thumbparams_Configurator\Thumbparams_Configurator\bin\Debug\net4.8.1-windows\win-x64\publish\Newtonsoft.Json.dll build/ThumbparamsOSC/Newtonsoft.Json.dll"
 cd %~dp0
 robocopy src/build build /MOVE /E /NFL /NDL /NJH /NJS /nc /ns /np
 cd build

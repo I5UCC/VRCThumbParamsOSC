4268c156f47e69051ffc0770988f099ede841226
Isaak <43730681+I5UCC@users.noreply.github.com>
2023-09-23 04:04:14 +0200

    Update setup.py

--- src/setup.py
+++ src/setup.py
@@ -2,7 +2,7 @@ import sys
 from cx_Freeze import setup, Executable
 
 packages = ["argparse", "async_timeout", "certifi", "charset_normalizer", "idna", "ifaddr", "lief", "openvr", "psutil", "pyparsing", "pythonosc", "requests", "urllib3", "zeroconf", "ctypes"]
-exclude = ["tkinter", "asyncio", "concurrent", "http", "lib2to3", "multiprocessing", "test", "unittest", "xmlrpc"]
+exclude = ["tkinter", "concurrent", "lib2to3", "test", "unittest", "xmlrpc"]
 file_include = ["config.json", "Run Debug Mode.bat", "bindings/", "app.vrmanifest"]
 bin_excludes = ["_bz2.pyd", "_decimal.pyd", "_hashlib.pyd", "_lzma.pyd", "_queue.pyd", "_ssl.pyd", "libcrypto-1_1.dll", "libssl-1_1.dll", "ucrtbase.dll", "VCRUNTIME140.dll"]
 

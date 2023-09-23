d40a8e56278a297f1b8e3c4c87f8d86c6705ed34
Isaak <43730681+I5UCC@users.noreply.github.com>
2023-09-23 03:32:42 +0200

    moved osc logic to osc.py

--- src/ThumbParamsOSC.py
+++ src/ThumbParamsOSC.py
@@ -6,13 +6,7 @@ import time
 import traceback
 import ctypes
 import argparse
-import zeroconf
-from pythonosc import udp_client, dispatcher, osc_server
-from tinyoscquery.queryservice import OSCQueryService
-from tinyoscquery.utility import get_open_tcp_port, get_open_udp_port, check_if_tcp_port_open, check_if_udp_port_open
-from tinyoscquery.query import OSCQueryBrowser, OSCQueryClient
-from psutil import process_iter
-from threading import Thread
+from osc import OSC
 
 
 def get_absolute_path(relative_path) -> str:
@@ -34,47 +28,6 @@ def cls() -> None:
         None
     """
     os.system('cls' if os.name == 'nt' else 'clear')
-
-
-def is_running() -> bool:
-    """
-    Checks if VRChat is running.
-    Returns:
-        bool: True if VRChat is running, False if not
-    """
-    _proc_name = "VRChat.exe" if os.name == 'nt' else "VRChat"
-    return _proc_name in (p.name() for p in process_iter())
-
-
-def wait_get_oscquery_client() -> OSCQueryClient:
-    """
-    Waits for VRChat to be discovered and ready and returns the OSCQueryClient.
-    Returns:
-        OSCQueryClient: OSCQueryClient for VRChat
-    """
-    service_info = None
-    print("Waiting for VRChat to be discovered.")
-    while service_info is None:
-        browser = OSCQueryBrowser()
-        time.sleep(2) # Wait for discovery
-        service_info = browser.find_service_by_name("VRChat")
-    print("VRChat discovered!")
-    client = OSCQueryClient(service_info)
-    print("Waiting for VRChat to be ready.")
-    while client.query_node(AVATAR_CHANGE_PARAMETER) is None:
-        time.sleep(2)
-    print("VRChat ready!")
-    return client
-
-
-def osc_server_serve() -> None:
-    """
-    Starts the OSC server.
-    Returns:
-        None
-    """
-    print(f"Starting OSC client on {IP}:{SERVER_PORT}:{HTTP_PORT}")
-    server.serve_forever(2)
 
 
 def get_debug_string(parameter, value, floating="", always = False) -> str:
@@ -180,175 +133,26 @@ def set_avatar_change(addr, value) -> None:
     Returns:
         None
     """
-    global curr_avatar
+    global osc
 
-    if curr_avatar == value:
+    if osc.curr_avatar == value:
         return
     
-    curr_avatar = value
+    osc.curr_avatar = value
 
     if config["ControllerType"]["enabled"]:
-        send_parameter("ControllerType", config["ControllerType"]["last_value"])
+        osc.send("ControllerType", config["ControllerType"]["last_value"])
     if config["LeftThumb"]["enabled"]:
-        send_parameter("LeftThumb", config["LeftThumb"]["last_value"])
+        osc.send("LeftThumb", config["LeftThumb"]["last_value"])
     if config["RightThumb"]["enabled"]:
-        send_parameter("RightThumb", config["RightThumb"]["last_value"])
+        osc.send("RightThumb", config["RightThumb"]["last_value"])
     if config["LeftABButtons"]["enabled"]:
-        send_parameter("LeftABButtons", config["LeftABButtons"]["last_value"])
+        osc.send("LeftABButtons", config["LeftABButtons"]["last_value"])
     if config["RightABButtons"]["enabled"]:
-        send_parameter("RightABButtons", config["RightABButtons"]["last_value"])
+        osc.send("RightABButtons", config["RightABButtons"]["last_value"])
 
     for action in actions:
-        send_osc(action, action["last_value"])
-
-
-def send_parameter(parameter: str, value) -> None:
-    """
-    Sends a parameter to VRChat via OSC.
-    Parameters:
-        parameter (str): Name of the parameter
-        value (any): Value of the parameter
-        floating (str): Floating value of the parameter, just for for debug output
-    Returns:
-        None
-    """
-    oscClient.send_message(AVATAR_PARAMETERS_PREFIX + parameter, value)
-
-
-def send_boolean_toggle(action: dict, value: bool) -> None:
-    """
-    Sends a boolean action as a toggle to VRChat via OSC.
-    Parameters:
-        action (dict): Action
-        value (bool): Value of the parameter
-    Returns:
-        None
-    """
-    if not action["enabled"]:
-        return
-    
-    if value:
-        action["last_value"] = not action["last_value"]
-        time.sleep(0.1)
-        send_parameter(action["osc_parameter"], action["last_value"])
-        return
-    
-    if action["always"]:
-        send_parameter(action["osc_parameter"], action["last_value"])
-
-
-def send_boolean(action: dict, value: bool) -> None:
-    """
-    Sends a boolean action to VRChat via OSC.
-    Parameters:
-        action (dict): Action
-        value (bool): Value of the parameter
-    Returns:
-        None
-    """
-    global curr_time
-
-    if not action["enabled"] or (not action["always"] and action["last_value"] == value):
-        return
-
-    if action["floating"]:
-        if value:
-            action["timestamp"] = curr_time
-        elif not value and curr_time - action["timestamp"] <= action["floating"]: 
-            value = action["last_value"]
-    
-    if not action["always"]:
-        action["last_value"] = value
-    else:
-        send_parameter(action["osc_parameter"], value)
-
-
-def send_vector1(action: dict, value: float) -> None:
-    """
-    Sends a vector1 action to VRChat via OSC.
-    Parameters:
-        action (dict): Action
-        value (float): Value of the parameter
-    Returns:
-        None
-    """
-    global curr_time
-
-    if not action["enabled"] or (not action["always"] and action["last_value"] == value):
-        return
-    
-    if action["floating"]:
-        if value > action["last_value"]:
-            action["timestamp"] = curr_time
-        elif value < action["last_value"] and curr_time - action["timestamp"] <= action["floating"]: 
-            value = action["last_value"]
-
-    if not action["always"]:
-        action["last_value"] = value
-    else:
-        send_parameter(action["osc_parameter"], value)
-
-
-def send_vector2(action: dict, value: tuple) -> None:
-    """
-    Sends a vector2 action to VRChat via OSC.
-    Parameters:
-        action (dict): Action
-        value (tuple): X and Y value of the parameter in a tuple
-    Returns:
-        None
-    """
-    global curr_time
-
-    val_x, val_y = value
-    tmp = (val_x > STICKTOLERANCE or val_y > STICKTOLERANCE) or (val_x < -STICKTOLERANCE or val_y < -STICKTOLERANCE)
-
-    if action["floating"]:
-        if val_x:
-            action["timestamp"][0] = curr_time
-        elif not val_x and curr_time - action["timestamp"][0] <= action["floating"][0]: 
-            val_x = action["last_value"][0]
-        if val_y:
-            action["timestamp"][1] = curr_time
-        elif not val_y and curr_time - action["timestamp"][1] <= action["floating"][1]:
-            val_y = action["last_value"][1]
-
-    if not action["always"]:
-        action["last_value"] = [val_x, val_y, tmp]
-
-    if action["enabled"][0] and (action["always"][0] or action["last_value"][0] != val_x):
-        send_parameter(action["osc_parameter"][0], val_x)
-    if action["enabled"][1] and (action["always"][1] or action["last_value"][1] != val_y):
-        send_parameter(action["osc_parameter"][1], val_y)
-    if len(action["osc_parameter"]) > 2 and action["enabled"][2] and (action["always"][2] or action["last_value"][2] != tmp):
-        send_parameter(action["osc_parameter"][2], tmp)
-
-
-def send_osc(action: dict | str, value: bool | float | tuple) -> None:
-    """
-    Sends an OSC message to VRChat.
-    Parameters:
-        action (dict | str): Action or OSC address
-        value (bool | float | tuple): Value of the parameter
-    Returns:
-        None
-    """
-    if isinstance(action, str):
-        send_parameter(action, value)
-        return
-    
-    match action['type']:
-        case "boolean":
-            if action["floating"] == -1:
-                send_boolean_toggle(action, value)
-            else:
-                send_boolean(action, value)
-        case "vector1":
-            send_vector1(action, value)
-        case "vector2":
-            send_vector2(action, value)
-        case _:
-            raise TypeError("Unknown action type: " + action['type'])
+        osc.send(action, action["last_value"])
 
 
 def handle_input() -> None:
@@ -357,7 +161,6 @@ def handle_input() -> None:
     Returns:
         None
     """
-    global curr_time
 
     _event = openvr.VREvent_t()
     _has_events = True
@@ -365,14 +168,14 @@ def handle_input() -> None:
         _has_events = application.pollNextEvent(_event)
     openvr.VRInput().updateActionState(actionsets)
 
-    curr_time = time.time()
+    osc.refresh_time()
 
     if config["ControllerType"]["enabled"]:
-        if curr_time - config["ControllerType"]["timestamp"] > 10.0:
+        if osc.curr_time - config["ControllerType"]["timestamp"] > 10.0:
             _controller_type = get_controllertype()
-            config["ControllerType"]["timestamp"] = curr_time
+            config["ControllerType"]["timestamp"] = osc.curr_time
             if config["ControllerType"]["last_value"] != _controller_type:
-                send_osc("ControllerType", _controller_type)
+                osc.send("ControllerType", _controller_type)
             else:
                 config["ControllerType"]["last_value"] = _controller_type
 
@@ -380,39 +183,39 @@ def handle_input() -> None:
     for action in actions[:8]: # Touch Actions
         val = get_value(action)
         _strinputs += "1" if val else "0"
-        send_osc(action, val)
+        osc.send(action, val)
 
     if config["LeftThumb"]["enabled"]:
         _leftthumb = _strinputs[:4].rfind("1") + 1
         if config["LeftThumb"]["last_value"] != _leftthumb or config["LeftThumb"]["always"]:
-            send_osc("LeftThumb", _leftthumb)
+            osc.send("LeftThumb", _leftthumb)
         else:
             config["LeftThumb"]["last_value"] = _leftthumb
 
     if config["RightThumb"]["enabled"]:
         _rightthumb = _strinputs[4:].rfind("1") + 1
         if config["RightThumb"]["last_value"] != _rightthumb or config["RightThumb"]["always"]:
-            send_osc("RightThumb", _rightthumb)
+            osc.send("RightThumb", _rightthumb)
         else:
             config["RightThumb"]["last_value"] = _rightthumb
 
     if config["LeftABButtons"]["enabled"]:
         _leftab = _strinputs[0] == "1" and _strinputs[1] == "1"
         if config["LeftABButtons"]["last_value"] != _leftab or config["LeftABButtons"]["always"]:
-            send_osc("LeftABButtons", _leftab)
+            osc.send("LeftABButtons", _leftab)
         else:
             config["LeftABButtons"]["last_value"] = _leftab
 
     if config["RightABButtons"]["enabled"]:
         _rightab = _strinputs[4] == "1" and _strinputs[5] == "1"
         if config["RightABButtons"]["last_value"] != _rightab or config["RightABButtons"]["always"]:
-            send_osc("RightABButtons", _rightab)
+            osc.send("RightABButtons", _rightab)
         else:
             config["RightABButtons"]["last_value"] = _rightab
 
     for action in actions[8:]:
         val = get_value(action)
-        send_osc(action, val)
+        osc.send(action, val)
 
     if args.debug:
         print_debugoutput()
@@ -447,60 +250,22 @@ actions = config["actions"]
 for action in actions:
     action["handle"] = openvr.VRInput().getActionHandle(action['name'])
 
-IP = args.ip if args.ip else config["IP"]
-PORT = args.port if args.port else config["Port"]
-SERVER_PORT = config["Server_Port"]
-HTTP_PORT = config["HTTP_Port"]
+config["IP"] = args.ip if args.ip else config["IP"]
+config["Port"] = args.port if args.port else config["Port"]
 POLLINGRATE = 1 / float(config['PollingRate'])
-STICKTOLERANCE = int(config['StickMoveTolerance']) / 100
-AVATAR_PARAMETERS_PREFIX = "/avatar/parameters/"
-AVATAR_CHANGE_PARAMETER = "/avatar/change"
-
-oscClient = udp_client.SimpleUDPClient(IP, PORT)
-
-disp = dispatcher.Dispatcher()
-disp.map(AVATAR_CHANGE_PARAMETER, set_avatar_change)
-
-if SERVER_PORT != 9001:
-    print("OSC Server port is not default, testing port availability and advertising OSCQuery endpoints")
-    if SERVER_PORT <= 0 or not check_if_udp_port_open(SERVER_PORT):
-        SERVER_PORT = get_open_udp_port()
-    if HTTP_PORT <= 0 or not check_if_tcp_port_open(HTTP_PORT):
-        HTTP_PORT = SERVER_PORT if check_if_tcp_port_open(SERVER_PORT) else get_open_tcp_port()
-else:
-    print("OSC Server port is default.")
-
-try:
-    print("Waiting for VRChat to start.")
-    while not is_running():
-        time.sleep(3)
-    print("VRChat started!")
-    qclient = wait_get_oscquery_client()
-    curr_avatar = qclient.query_node(AVATAR_CHANGE_PARAMETER).value[0]
-    server = osc_server.ThreadingOSCUDPServer((IP, SERVER_PORT), disp)
-    server_thread = Thread(target=osc_server_serve, daemon=True)
-    server_thread.start()
-    oscqs = OSCQueryService("ThumbParamsOSC", HTTP_PORT, SERVER_PORT)
-    oscqs.advertise_endpoint(AVATAR_CHANGE_PARAMETER, access="readwrite")
-except OSError as e:
-    print("You can only bind to the port 9001 once.")
-    print(traceback.format_exc())
-    if os.name == "nt":
-        ctypes.windll.user32.MessageBoxW(0, "You can only bind to the port 9001 once.", "AvatarParameterSync - Error", 0)
-    sys.exit(1)
-except zeroconf._exceptions.NonUniqueNameException as e:
-    print.error("NonUniqueNameException, trying again...")
-    os.execv(sys.executable, ['python'] + sys.argv)
+osc = OSC(config, set_avatar_change)
 
 cls()
 if not args.debug:
     print("ThumbParamsOSC running...\n")
     print("You can minimize this window.\n")
     print("Press CTRL+C to exit.\n")
-    print(f"IP:\t\t\t{IP}")
-    print(f"Port:\t\t\t{PORT}")
+    print(f"IP:\t\t\t{osc.ip}")
+    print(f"Port:\t\t\t{osc.port}")
+    print(f"Server Port:\t\t{osc.server_port}")
+    print(f"HTTP Port:\t\t{osc.http_port}")
     print(f"PollingRate:\t\t{POLLINGRATE}s ({config['PollingRate']} Hz)")
-    print(f"StickMoveTolerance:\t{STICKTOLERANCE} ({config['StickMoveTolerance']}%)")
+    print(f"StickMoveTolerance:\t{osc.stick_tolerance} ({config['StickMoveTolerance']}%)")
     print("\nOpen Configurator.exe to change sent Parameters and other Settings.")
 
 # Main Loop
---
+++ src/osc.py
@@ -0,0 +1,254 @@
+from pythonosc import udp_client, dispatcher, osc_server
+from tinyoscquery.queryservice import OSCQueryService
+from tinyoscquery.utility import get_open_tcp_port, get_open_udp_port, check_if_tcp_port_open, check_if_udp_port_open
+from tinyoscquery.query import OSCQueryBrowser, OSCQueryClient
+import time
+import os
+import sys
+import ctypes
+import traceback
+from threading import Thread
+from psutil import process_iter
+import zeroconf
+
+AVATAR_PARAMETERS_PREFIX = "/avatar/parameters/"
+AVATAR_CHANGE_PARAMETER = "/avatar/change"
+
+class OSC:
+    def __init__(self, conf: dict, avatar_change_function) -> None:
+        self.ip = conf["IP"]
+        self.port = conf["Port"]
+        self.server_port = conf["Server_Port"]
+        self.http_port = conf["HTTP_Port"]
+        self.stick_tolerance = int(conf['StickMoveTolerance']) / 100
+        self.curr_avatar = ""
+        self.curr_time = 0
+
+        self.oscClient = udp_client.SimpleUDPClient(self.ip, self.port)
+        self.disp = dispatcher.Dispatcher()
+        self.disp.map(AVATAR_CHANGE_PARAMETER, avatar_change_function)
+        if self.server_port != 9001:
+            print("OSC Server port is not default, testing port availability and advertising OSCQuery endpoints")
+            if self.server_port <= 0 or not check_if_udp_port_open(self.server_port):
+                self.server_port = get_open_udp_port()
+            if self.http_port <= 0 or not check_if_tcp_port_open(self.http_port):
+                self.http_port = self.server_port if check_if_tcp_port_open(self.server_port) else get_open_tcp_port()
+        else:
+            print("OSC Server port is default.")
+
+        try:
+            print("Waiting for VRChat to start.")
+            while not self.is_running():
+                time.sleep(3)
+            print("VRChat started!")
+            self.qclient = self._wait_get_oscquery_client()
+            self.curr_avatar = self.qclient.query_node(AVATAR_CHANGE_PARAMETER).value[0]
+            self.server = osc_server.ThreadingOSCUDPServer((self.ip, self.server_port), self.disp)
+            server_thread = Thread(target=self._osc_server_serve, daemon=True)
+            server_thread.start()
+            self.oscqs = OSCQueryService("ThumbParamsOSC", self.http_port, self.server_port)
+            self.oscqs.advertise_endpoint(AVATAR_CHANGE_PARAMETER, access="readwrite")
+        except OSError as e:
+            print("You can only bind to the port 9001 once.")
+            print(traceback.format_exc())
+            if os.name == "nt":
+                ctypes.windll.user32.MessageBoxW(0, "You can only bind to the port 9001 once.", "AvatarParameterSync - Error", 0)
+            sys.exit(1)
+        except zeroconf._exceptions.NonUniqueNameException as e:
+            print.error("NonUniqueNameException, trying again...")
+            os.execv(sys.executable, ['python'] + sys.argv)
+
+
+    def is_running(self) -> bool:
+        """
+        Checks if VRChat is running.
+        Returns:
+            bool: True if VRChat is running, False if not
+        """
+        _proc_name = "VRChat.exe" if os.name == 'nt' else "VRChat"
+        return _proc_name in (p.name() for p in process_iter())
+
+
+    def _wait_get_oscquery_client(self) -> OSCQueryClient:
+        """
+        Waits for VRChat to be discovered and ready and returns the OSCQueryClient.
+        Returns:
+            OSCQueryClient: OSCQueryClient for VRChat
+        """
+        service_info = None
+        print("Waiting for VRChat to be discovered.")
+        while service_info is None:
+            browser = OSCQueryBrowser()
+            time.sleep(2) # Wait for discovery
+            service_info = browser.find_service_by_name("VRChat")
+        print("VRChat discovered!")
+        client = OSCQueryClient(service_info)
+        print("Waiting for VRChat to be ready.")
+        while client.query_node(AVATAR_CHANGE_PARAMETER) is None:
+            time.sleep(2)
+        print("VRChat ready!")
+        return client
+
+
+    def _osc_server_serve(self) -> None:
+        """
+        Starts the OSC server.
+        Returns:
+            None
+        """
+        print(f"Starting OSC client on {self.ip}:{self.server_port}:{self.http_port}")
+        self.server.serve_forever(2)
+
+
+    def _send_parameter(self, parameter: str, value) -> None:
+        """
+        Sends a parameter to VRChat via OSC.
+        Parameters:
+            parameter (str): Name of the parameter
+            value (any): Value of the parameter
+            floating (str): Floating value of the parameter, just for for debug output
+        Returns:
+            None
+        """
+        self.oscClient.send_message(AVATAR_PARAMETERS_PREFIX + parameter, value)
+
+
+    def _send_boolean_toggle(self, action: dict, value: bool) -> None:
+        """
+        Sends a boolean action as a toggle to VRChat via OSC.
+        Parameters:
+            action (dict): Action
+            value (bool): Value of the parameter
+        Returns:
+            None
+        """
+        if not action["enabled"]:
+            return
+        
+        if value:
+            action["last_value"] = not action["last_value"]
+            time.sleep(0.1)
+            self._send_parameter(action["osc_parameter"], action["last_value"])
+            return
+        
+        if action["always"]:
+            self._send_parameter(action["osc_parameter"], action["last_value"])
+
+
+    def _send_boolean(self, action: dict, value: bool) -> None:
+        """
+        Sends a boolean action to VRChat via OSC.
+        Parameters:
+            action (dict): Action
+            value (bool): Value of the parameter
+        Returns:
+            None
+        """
+
+        if not action["enabled"] or (not action["always"] and action["last_value"] == value):
+            return
+
+        if action["floating"]:
+            if value:
+                action["timestamp"] = self.curr_time
+            elif not value and self.curr_time - action["timestamp"] <= action["floating"]: 
+                value = action["last_value"]
+        
+        if not action["always"]:
+            action["last_value"] = value
+        else:
+            self._send_parameter(action["osc_parameter"], value)
+
+
+    def _send_vector1(self, action: dict, value: float) -> None:
+        """
+        Sends a vector1 action to VRChat via OSC.
+        Parameters:
+            action (dict): Action
+            value (float): Value of the parameter
+        Returns:
+            None
+        """
+
+        if not action["enabled"] or (not action["always"] and action["last_value"] == value):
+            return
+        
+        if action["floating"]:
+            if value > action["last_value"]:
+                action["timestamp"] = self.curr_time
+            elif value < action["last_value"] and self.curr_time - action["timestamp"] <= action["floating"]: 
+                value = action["last_value"]
+
+        if not action["always"]:
+            action["last_value"] = value
+        else:
+            self._send_parameter(action["osc_parameter"], value)
+
+
+    def _send_vector2(self, action: dict, value: tuple) -> None:
+        """
+        Sends a vector2 action to VRChat via OSC.
+        Parameters:
+            action (dict): Action
+            value (tuple): X and Y value of the parameter in a tuple
+        Returns:
+            None
+        """
+        val_x, val_y = value
+        tmp = (val_x > self.stick_tolerance or val_y > self.stick_tolerance) or (val_x < -self.stick_tolerance or val_y < -self.stick_tolerance)
+
+        if action["floating"]:
+            if val_x:
+                action["timestamp"][0] = self.curr_time
+            elif not val_x and self.curr_time - action["timestamp"][0] <= action["floating"][0]: 
+                val_x = action["last_value"][0]
+            if val_y:
+                action["timestamp"][1] = self.curr_time
+            elif not val_y and self.curr_time - action["timestamp"][1] <= action["floating"][1]:
+                val_y = action["last_value"][1]
+
+        if not action["always"]:
+            action["last_value"] = [val_x, val_y, tmp]
+
+        if action["enabled"][0] and (action["always"][0] or action["last_value"][0] != val_x):
+            self._send_parameter(action["osc_parameter"][0], val_x)
+        if action["enabled"][1] and (action["always"][1] or action["last_value"][1] != val_y):
+            self._send_parameter(action["osc_parameter"][1], val_y)
+        if len(action["osc_parameter"]) > 2 and action["enabled"][2] and (action["always"][2] or action["last_value"][2] != tmp):
+            self._send_parameter(action["osc_parameter"][2], tmp)
+
+
+    def refresh_time(self) -> None:
+        """
+        Refreshes the current time.
+        Returns:
+            None
+        """
+        self.curr_time = time.time()
+
+
+    def send(self, action: dict | str, value: bool | float | tuple) -> None:
+        """
+        Sends an OSC message to VRChat.
+        Parameters:
+            action (dict | str): Action or OSC address
+            value (bool | float | tuple): Value of the parameter
+        Returns:
+            None
+        """
+        if isinstance(action, str):
+            self._send_parameter(action, value)
+            return
+        
+        match action['type']:
+            case "boolean":
+                if action["floating"] == -1:
+                    self._send_boolean_toggle(action, value)
+                else:
+                    self._send_boolean(action, value)
+            case "vector1":
+                self._send_vector1(action, value)
+            case "vector2":
+                self._send_vector2(action, value)
+            case _:
+                raise TypeError("Unknown action type: " + action['type'])

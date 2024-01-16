04350bd6eea51ab74b463d63c7bd930b188d1c0f
Isaak <I5UCC@pm.me>
2024-01-16 20:43:00 +0100

    Update build.bat
    now using 3.11

--- build.bat
+++ build.bat
@@ -10,7 +10,7 @@ dotnet publish -r win-x64
 cd ..
 "../.venv/Scripts/python.exe" setup.py build
 cd build
-robocopy exe.win-amd64-3.10 ThumbparamsOSC /MOVE /E /NFL /NDL /NJH /NJS /nc /ns /np
+robocopy exe.win-amd64-3.11 ThumbparamsOSC /MOVE /E /NFL /NDL /NJH /NJS /nc /ns /np
 cd ..
 powershell -c "copy Thumbparams_Configurator\Thumbparams_Configurator\bin\Debug\net4.8.1-windows\win-x64\publish\Thumbparams_Configurator.exe build/ThumbparamsOSC/Configurator.exe"
 powershell -c "copy Thumbparams_Configurator\Thumbparams_Configurator\bin\Debug\net4.8.1-windows\win-x64\publish\Newtonsoft.Json.dll build/ThumbparamsOSC/Newtonsoft.Json.dll"
